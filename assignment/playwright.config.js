a95a99fb57fd9d37fc2c7cbbc035028d09110ae2
SidMehta31 <sidmehta07@gmail.com>
2025-08-27 17:51:04 +0530

    Update playwright.config.js
    code refactor

--- assignment/playwright.config.js
+++ assignment/playwright.config.js
@@ -1,13 +1,14 @@
 const settings = require('./setting.json');
 
 module.exports = {
-  timeout: 60 * 1000
+  timeout: 60 * 1000,
   use: {
     headless: !settings.headed,
     baseURL: settings.baseURL,
-    navigationTimeout: 60 * 1000
+    navigationTimeout: 60 * 1000,
     screenshot: 'only-on-failure'
   },
   workers: settings.executionMode === "parallel" ? undefined : 1
 };
+
 
