a24472f32a50418aaf609ae1a480d20ab5097677
SidMehta31 <sidmehta07@gmail.com>
2025-08-27 17:29:03 +0530

    Update cartValidationTest.spec.js
    updating path.

--- assignment/tests/ui/cartValidationTest.spec.js
+++ assignment/tests/ui/cartValidationTest.spec.js
@@ -1,7 +1,7 @@
 const { test, expect } = require('@playwright/test');
-const { CartPage } = require('../../pom/CartPagePOM');
-const { LoginPage } = require('../../pom/LoginPagePOM');
-const { readData } = require('../../utils/fileHelper');
+const { CartPage } = require('../pom/CartPagePOM');
+const { LoginPage } = require('../pom/LoginPagePOM');
+const { readData } = require('../utils/fileHelper');
 
 test('Validate items in cart using previous test outcome', async ({ page }) => {
   const loginPage = new LoginPage(page);
@@ -14,4 +14,5 @@ test('Validate items in cart using previous test outcome', async ({ page }) => {
 
   const count = await cartPage.getCartItemsCount();
   expect(count).toBe(parseInt(cartData.cartCount));
+
 });
